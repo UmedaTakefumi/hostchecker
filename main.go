302e93704db999a1e11799339bbf342b151d8fe0
UmedaTakefumi <ut+github@azurestone.net>
2017-07-08 05:50:34 +0900

    ipアドレスの出力形式を変更

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
   "net"
 	"net/url"
 	//  "os"
-	//  "reflect"
+  //"reflect"
 )
 
 func main() {
@@ -27,6 +27,20 @@ func main() {
   if err != nil {
     log.Fatal(err)
   }
-  fmt.Printf("IPAddress: %s\n", i)
+  fmt.Println("IPAddress:")
+//  fmt.Println(reflect.TypeOf(i))
+
+  for k, v := range i{
+    fmt.Printf("  %d: %s\n", k, v)
+  }
+
+
+//  uh, err := net.LookupIP(i)
+//  if err != nil {
+//    log.Fatal(err)
+//  }
+//
+//  fmt.Printf("A recoard: %s\n", uh)
+
 
 }
